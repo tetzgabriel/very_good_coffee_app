afc25e4405731f8aa98eb782027e83bfeb3e7b31
Gabriel Tetzlaf Mansano <tetz.gabriel@gmail.com>
2023-02-15 08:35:12 -0300

    Fix | Adding FavoritesCubit mock

---
+++ test/mocks/mock_favorites_cubit.dart
@@ -0,0 +1,5 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:very_good_coffee_app/favorites/favorites.dart';
+
+class MockFavoritesCubit extends MockCubit<FavoritesState>
+    implements FavoritesCubit {}
