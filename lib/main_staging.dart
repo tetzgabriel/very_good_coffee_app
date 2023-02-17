d5cac31f2fbb66c71c3d2c0da6da1438c2aa5881
Gabriel Tetzlaf Mansano <tetz.gabriel@gmail.com>
2023-02-16 21:09:29 -0300

    Fix | Flavors and format

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:very_good_coffee_app/app/app.dart';
+import 'package:very_good_coffee_app/app/injectable/injectable.dart';
 import 'package:very_good_coffee_app/bootstrap.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setupDependencies();
+
   bootstrap(() => const App());
 }
